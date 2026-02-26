3c6e904a9c5767a19ee11a4ad6e65da90514f805
RizwanMustafa <rizwan@devnaza.com>
2026-02-26 14:27:09 +0500

    testing

--- backend/src/routes/category.routes.js
+++ backend/src/routes/category.routes.js
@@ -8,5 +8,4 @@ router.get("/", requireAuth, category.getAll);
 router.post("/", requireAuth, category.create);
 router.put("/:id", requireAuth, category.update);
 router.delete("/:id", requireAuth, category.remove);
-
 module.exports = router;
