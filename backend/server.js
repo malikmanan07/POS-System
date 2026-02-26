6624c3577da6ceba9bec36dd77f5e1cf31d3aa77
Ahmed <ahmed@devnaza.com>
2026-02-26 13:42:23 +0500

    backend changes

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,9 @@ const categoryRoutes = require("./src/routes/category.routes");
 const customerRoutes = require("./src/routes/customer.routes");
 const saleRoutes = require("./src/routes/sale.routes");
 const dashboardRoutes = require("./src/routes/dashboard.routes");
-<<<<<<< HEAD
 const settingsRoutes = require("./src/routes/settingsRoutes");
 
-=======
 const stockRoutes = require("./src/routes/stock.routes");
->>>>>>> Asifwebdev
 
 const app = express();
 
@@ -31,12 +28,9 @@ app.use("/api/categories", categoryRoutes);
 app.use("/api/customers", customerRoutes);
 app.use("/api/sales", saleRoutes);
 app.use("/api/dashboard", dashboardRoutes);
-<<<<<<< HEAD
 app.use("/api/settings", settingsRoutes);
 
-=======
 app.use("/api/stock", stockRoutes);
->>>>>>> Asifwebdev
 
 app.get("/db-test", async (req, res) => {
   try {
